7bb48dd1b8f7f85e7cacd73e770c898e72ada3da
fico45 <novosel.filip45@gmail.com>
2023-05-10 18:39:40 +0200

    feat(WordCheckFormatter): Check all Croatian multiple-letter letters

--- bin/kaladont/services/word_check_formatter.dart
+++ bin/kaladont/services/word_check_formatter.dart
@@ -2,16 +2,28 @@ class WordCheckFormatter {
   static String getFirstTwoLetters({required String word}) {
     String wordToCheck = word.toLowerCase();
     String substring = wordToCheck.substring(0, 3);
-    bool containsNJ = substring.contains('nj');
-    return containsNJ ? substring : wordToCheck.substring(0, 2);
+    bool hasLast3Letters = substring.contains('nj');
+    return hasLast3Letters ? substring : wordToCheck.substring(0, 2);
   }
 
   static String getLastTwoLetters({required String word, required int length}) {
     String wordToCheck = word.toLowerCase();
     String substring = wordToCheck.substring(length - 2, length + 1);
-    bool containsNJ = substring.contains('nj');
-    return containsNJ
+    bool hasLast3Letters = containsLast3Letters(substring: substring);
+    return hasLast3Letters
         ? substring
         : wordToCheck.substring(length - 1, length + 1);
+  }
+
+  static bool containsLast3Letters({required String substring}) {
+    bool contains = false;
+    if (substring.contains('nj')) {
+      contains = true;
+    } else if (substring.contains('lj')) {
+      contains = true;
+    } else if (substring.contains('dž')) {
+      contains = true;
+    }
+    return contains;
   }
 }
