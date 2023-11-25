bcdc34d2702e9b64871b6577af097e8240f46672
fico45 <novosel.filip45@gmail.com>
2023-11-25 11:24:35 +0100

    feat(MessageColor): Encapsulate DiscordColors

---
+++ bin/kaladont/message_colors.dart
@@ -0,0 +1,12 @@
+import 'package:nyxx/nyxx.dart';
+
+class MessageColor {
+  static const red = DiscordColor.fromRgb(255, 0, 0);
+  static const green = DiscordColor.fromRgb(0, 255, 0);
+  static const yellow = DiscordColor.fromRgb(255, 255, 0);
+  static const blue = DiscordColor.fromRgb(0, 0, 255);
+  static const magenta = DiscordColor.fromRgb(255, 0, 255);
+  static const cyan = DiscordColor.fromRgb(0, 255, 255);
+  static const white = DiscordColor.fromRgb(255, 255, 255);
+  static const turquoise = DiscordColor.fromRgb(64, 224, 208);
+}
